b191f827c056395bbf823e982d116c47f9a9c796
Thomas Clark <tnc1997@virginmedia.com>
2018-01-20 12:32:41 +0000

    feat(avon): create avon application and add home page

---
+++ apps/avon/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home',
+  },
+  {
+    loadChildren: './home/home.module#HomeModule',
+    path: 'home',
+  },
+];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(ROUTES) ],
+})
+export class AppRoutingModule {}
---
+++ apps/avon/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NxModule } from '@nrwl/nx';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  declarations: [AppComponent],
+  imports: [AppRoutingModule, BrowserModule, NxModule.forRoot()]
+})
+export class AppModule {}
